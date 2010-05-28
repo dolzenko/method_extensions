00a1c51ee3adf218480158553ec61e00b7e40cd4
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-05-28 10:44:51 +0400

    Don't require method/super by default

--- lib/method_extensions.rb
+++ lib/method_extensions.rb
@@ -1,2 +1,3 @@
 require "method_extensions/method/source_with_doc"
-require "method_extensions/method/super"
+# We don't require `super` by default since it patches core methods
+# require "method_extensions/method/super"
