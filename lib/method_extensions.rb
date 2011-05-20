b8358ed234112486f8ded481e5e56e7e111fca09
Bryce Kerley <bkerley@brycekerley.net>
2011-05-20 16:33:29 -0400

    add SourceNotFoundError class

--- lib/method_extensions.rb
+++ lib/method_extensions.rb
@@ -1,3 +1,4 @@
 require "method_extensions/method/source_with_doc"
+require "method_extensions/source_not_found_error"
 # We don't require `super` by default since it patches core methods
 # require "method_extensions/method/super"
